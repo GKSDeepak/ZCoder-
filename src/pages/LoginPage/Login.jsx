876ca9c8bdbeaafe674ebb17b00d83aca88337dd
Abhijith <jajamabhijith2004@gmail.com>
2024-05-31 20:40:58 +0530

    added border to nav,updated readme,stored the input for login and signup

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FaUser,FaLock } from "react-icons/fa"
 // import './Login.css'
 import styles from './Login.module.css'
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [user,setUser] = useState({
+    username:"",
+    password:"",
+  });
+  const handleInput=(e)=>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({...user,
+      [name]:value,
+    })
+  };
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    //console.log(user);
+    navigate("/");
+  };
+
   return (
     <section className={styles.sectionForm}>
       
         <div className={styles.wrapper}>
           <h1>LOGIN</h1>
-          <form action="">
+          <form onSubmit={handleSubmit} action="">
             <div className={styles.inputBox}>
-              <input className={styles.input} type="text" placeholder='Username' required />
+              <input name="username" className={styles.input} type="text" placeholder='Username' required value={user.username} onChange={handleInput} />
               <FaUser className={styles.icon}/>
             </div>
             <div className={styles.inputBox}>
-              <input type="password" placeholder='Password' required />
+              <input name="password" type="password" placeholder='Password' required value={user.password} onChange={handleInput} />
               <FaLock className={styles.icon} />
             </div>
         
-              <button type="submit" className={styles.loginButton} onClick={()=>navigate('/')}>Login</button>
+              <button type="submit" className={styles.loginButton}>Login</button>
             
             <div className={styles.registerLink}>
               <p>Don't have an account? <a href="./signup">Signup</a></p>
--- src/pages/SignupPage/Signup.jsx
+++ src/pages/SignupPage/Signup.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FaUser,FaLock } from "react-icons/fa"
 import { MdEmail } from "react-icons/md"
 import styles from './Signup.module.css'
 const Signup = () => {
+  const navigate = useNavigate();
+  const [user,setUser] = useState({
+    username:"",
+    email:"",
+    password:"",
+  });
+  const handleInput=(e)=>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({...user,
+      [name]:value,
+    })
+  };
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+   // console.log(user);
+    navigate("/");
+  };
+
   return (
     <section className={styles.sectionForm}>
       
       <div className={styles.signupContent}>
       <h1>Signup</h1>
-        <form action="">
+        <form onSubmit={handleSubmit} action="">
           
           <div className={styles.inputBox}>
             {/* <label htmlFor="">username</label> */}
-            <input className={styles.input} type="text" name="username" placeholder='Username' required />
+            <input name="username" className={styles.input} type="text"  placeholder='Username' required value={user.name} onChange={handleInput} />
             <FaUser className={styles.icon}/>
           </div>
           <div className={styles.inputBox}>
             {/* <label htmlFor="">Email</label> */}
-            <input className={styles.input} type="email" placeholder='Email' required />
+            <input name="email" className={styles.input} type="email" placeholder='Email' required value={user.email} onChange={handleInput} />
             <MdEmail className={styles.icon}/>
           </div>
           <div className={styles.inputBox}>
             {/* <label htmlFor="">Password</label> */}
-            <input className={styles.input} type="password" placeholder='Password' required />
+            <input name="password" className={styles.input} type="password" placeholder='Password' required value={user.password} onChange={handleInput} />
             <FaLock className={styles.icon} />
           </div>
           <button type="submit" className={styles.signupButton}>Signup</button>
