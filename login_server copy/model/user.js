34a3a8d0b7611fcbff048e9cf320ea3134e5f0d7
GKSDeepak <deepakgubbala23@gmail.com>
2024-06-04 23:04:34 +0530

    added login server

---
+++ login_server copy/model/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    dateOfBirth:Date,
+    verified:Boolean
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
